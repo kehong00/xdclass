e4176bc0dd779b5ad6b8ce7f4c81ed29cfb05b39
kehong00 <kehong00@foxmail.com>
2020-09-14 20:17:34 +0800

    Initial commit

---
+++ src/main/java/cn/codewoo/XdclassApplication.java
@@ -0,0 +1,13 @@
+package cn.codewoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codewoo/XdclassApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codewoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
